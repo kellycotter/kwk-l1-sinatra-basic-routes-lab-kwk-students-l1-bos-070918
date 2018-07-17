cbddab3073903ab9bf2d30cbbe794909e9f3897c
Kelly Cotter <kjc853@gmail.com>
2018-07-17 14:01:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,12 +5,12 @@ class App < Sinatra::Base
     "Hello, World"
   end
   get '/name' do
-    "My name is _"
+    "My name is "
   end
   get '/hometown' do
-    "My hometown is_"
+    "My hometown is"
   end
   get '/favorite-song' do
-  "My favorite song is_"
+  "My favorite song is"
 end
 end
